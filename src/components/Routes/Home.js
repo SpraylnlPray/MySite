2291af831fcd042a5d1cbcaa83d08dcd5497d16b
SpraylnlPray <danielwildegger@gmx.de>
2020-09-09 22:01:50 +0200

    Start page

--- src/components/Routes/Home.js
+++ src/components/Routes/Home.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../css/mobile/Home.css';
 
 const Home = () => {
   return (
-    <div>Home Route</div>
+    <div id='mobile-home-container'>
+      <div id='hi'>Hi,</div>
+      <div id='mobile-presentation'>
+        I&apos;m Daniel.<br />
+        A CS student<br />
+        from Germany.
+      </div>
+      <div id='mobile-skills'>
+        JavaScript, React, C++, C#, Apollo, GraphQL, Neo4j
+      </div>
+      <Link to='/contact'>
+        <button id='mobile-touch-button'>Get in Touch</button>
+      </Link>
+    </div>
   );
 }
 
