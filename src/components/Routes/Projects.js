2b03983f5666d8c25ac8c42c6652dbcfceed4fda
SpraylnlPray <danielwildegger@gmx.de>
2020-09-17 16:50:58 +0200

    Projects page

--- src/components/Routes/Projects.js
+++ src/components/Routes/Projects.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import '../../css/mobile/Projects.css';
+import projects from '../../static/Projects';
 
 const Projects = () => {
+
+  const projectElements = projects.map( project => {
+    return (
+      <div key={project.key} className='mobile-project-container'>
+        <div className='mobile-project-title'>{project.title}</div>
+        <div className='mobile-project-sub'>{project.sub}</div>
+        <div className='mobile-project-description'>{project.description}</div>
+        <a href={project.source} download={project.source_type}>{project.source_text}</a>
+      </div>
+    );
+  } );
+
   return (
-    <div>Projects Route</div>
+    <div id='mobile-projects-container'>
+      <div id='mobile-projects-header'>My Work</div>
+      <div id='mobile-projects-caption'>A list of things I created during the years. Documents as well as Code. For more visit my <a href='https://github.com/SpraylnlPray'>GitHub</a>.</div>
+      <div id='mobile-projects-list'>
+        {projectElements}
+      </div>
+    </div>
   );
 }
 
--- src/static/Projects.js
+++ src/static/Projects.js
@@ -4,18 +4,27 @@ const projects = [
     title: 'Cybercell Game',
     sub: '4th Semester, Game Engineering Class',
     description: 'This is the very first "game" I wrote together with a friend. Its a round based shooter, playing in the future. Written in C# and Unity it was the first experience of working on the same project for longer than a week.',
+    source: 'https://www.youtube.com/watch?v=bo3Ef76Esog&feature=youtu.be',
+    source_text: 'Video',
+    download: false,
   },
   {
     key: 'nn-sem',
     title: 'Backpropagation und -varianten',
     sub: '6th Semester, Seminar Work',
     description: 'In this paper I explain the idea behind backpropagation - the magic that makes neural networks learn - and also look at some variations of the basic implementation, that aim to fix some of its problems. (German only)',
+    source: '../../../PDF/Seminararbeit.pdf',
+    source_text: 'Download',
+    download: true,
   },
   {
     key: 'yc-course',
     title: 'YelpCamp',
     sub: '7th Semester, Udemy Course',
     description: 'This website was the result of a course on Udemy that introduced me to web development and tought me the basics of HTML, CSS and JavaScript. The site is a place to leave reviews on fictive camping sites. It has authentication and authorization and is connected to a mongo DB.',
+    source: 'https://young-fortress-00311.herokuapp.com/',
+    source_text: 'Link',
+    download: false,
   }
 ];
 
