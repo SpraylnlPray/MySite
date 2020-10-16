5a553fd2938b6bd40269005d1d9f5e3b68e57e3c
SpraylnlPray <danielwildegger@gmx.de>
2020-10-16 17:32:44 +0200

    update sitemap

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
-const SitemapPlugin = require( 'sitemap-webpack-plugin' ).default;
-const path = require( 'path' );
-const webpack = require( 'webpack' );
-const dotenv = require( 'dotenv' );
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
+const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 dotenv.config().parsed;
@@ -10,46 +10,45 @@ dotenv.config().parsed;
 const paths = [
   {
     path: '/',
-    lastmod: '2020-09-21',
-    priority: '0.5',
-    changefreq: 'monthly'
+    lastmod: '2020-16-10',
+    priority: '0.6',
+    changefreq: 'monthly',
   },
   {
     path: '/about',
     lastmod: '2020-09-21',
     priority: '0.6',
-    changefreq: 'weekly'
+    changefreq: 'weekly',
   },
   {
     path: '/projects',
     lastmod: '2020-09-23',
     priority: '0.8',
-    changefreq: 'weekly'
+    changefreq: 'weekly',
   },
   {
     path: '/experience',
     lastmod: '2020-09-21',
     priority: '0.8',
-    changefreq: 'weekly'
+    changefreq: 'weekly',
   },
   {
     path: '/contact',
     lastmod: '2020-09-21',
     priority: '0.3',
-    changefreq: 'monthly'
+    changefreq: 'monthly',
   },
 ];
 
-
 module.exports = () => {
-  const envKeys = Object.keys( process.env ).reduce( ( prev, next ) => {
-    prev[ `process.env.${ next }` ] = JSON.stringify( process.env[ next ] );
+  const envKeys = Object.keys(process.env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(process.env[next]);
     return prev;
-  }, {} );
+  }, {});
   return {
     entry: './src/index.js',
     output: {
-      path: path.join( __dirname, '/dir' ),
+      path: path.join(__dirname, '/dir'),
       filename: 'bundle.js',
       publicPath: '/',
     },
@@ -58,17 +57,17 @@ module.exports = () => {
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          loader: 'babel-loader'
+          loader: 'babel-loader',
         },
         {
           test: /\.s[ac]ss$/i,
-          use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+          use: ['style-loader', 'css-loader', 'sass-loader'],
         },
         {
           test: /\.(jpe?g|gif|png|svg|woff|ttf|wav|mp3|pdf)$/,
           loader: 'file-loader',
         },
-      ]
+      ],
     },
     devtool: 'source-map',
     devServer: {
@@ -78,19 +77,22 @@ module.exports = () => {
       historyApiFallback: true,
     },
     plugins: [
-      new HtmlWebPackPlugin( {
-        template: "./src/index.html",
-        filename: "./index.html"
-      } ),
-      new webpack.DefinePlugin( envKeys ),
-      new SitemapPlugin( 'https://danielwildegger.com', paths ),
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+        filename: './index.html',
+      }),
+      new webpack.DefinePlugin(envKeys),
+      new SitemapPlugin('https://danielwildegger.com', paths),
       new CopyWebpackPlugin({
         patterns: [
-          {from: './robots.txt', to: './robots.txt' },
-          {from: './icons/favicon-small.png', to: './icons/favicon-small.png'},
-          {from: './icons/favicon.png', to: './icons/favicon.png'}
-        ]
-      })
-    ]
+          { from: './robots.txt', to: './robots.txt' },
+          {
+            from: './icons/favicon-small.png',
+            to: './icons/favicon-small.png',
+          },
+          { from: './icons/favicon.png', to: './icons/favicon.png' },
+        ],
+      }),
+    ],
   };
-}
+};
