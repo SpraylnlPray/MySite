0f569c81d86f1abff94cb704c22c71dda8bc869c
SpraylnlPray <danielwildegger@gmx.de>
2020-11-06 19:28:18 +0100

    Switch to CRA

--- src/static/Jobs.js
+++ src/static/Jobs.js
@@ -1,31 +1,33 @@
-const codesysLogo = require('../../icons/Logos-Big/CODESYS-logo-standard.png');
-const ioetLogo = require('../../icons/ioetLogo4x6.png');
+const codesysLogo = require("../icons/Logos-Big/CODESYS-logo-standard.png");
+const ioetLogo = require("../icons/ioetLogo4x6.png");
 
 const jobs = [
   {
-    key: 'codesys',
-    time: 'Mar.19 - Mar.20',
-    header: 'CODESYS GmbH',
-    frontSub: 'Kempten Germany',
-    backSub: '8h/Week Intern',
-    description: 'Architectural refactoring of an internal tool and porting it from Python to C#, as well as setting up various Raspberry-Pi for testing purposes',
+    key: "codesys",
+    time: "Mar.19 - Mar.20",
+    header: "CODESYS GmbH",
+    frontSub: "Kempten Germany",
+    backSub: "8h/Week Intern",
+    description:
+      "Architectural refactoring of an internal tool and porting it from Python to C#, as well as setting up various Raspberry-Pi for testing purposes",
     image: codesysLogo,
-    ref: 'https://www.codesys.com/',
+    ref: "https://www.codesys.com/",
     download: false,
-    refText: 'Web',
+    refText: "Web",
   },
   {
-    key: 'ioet',
-    time: 'Sep.18 - Feb.19',
-    header: 'ioet Inc.',
-    frontSub: 'Quito Ecuador',
-    backSub: 'Fulltime Intern',
-    description: 'Backend Development in Python 3, implemented an algorithm that solves a spacial allocation problem and deployed it to an AWS lambda function',
+    key: "ioet",
+    time: "Sep.18 - Feb.19",
+    header: "ioet Inc.",
+    frontSub: "Quito Ecuador",
+    backSub: "Fulltime Intern",
+    description:
+      "Backend Development in Python 3, implemented an algorithm that solves a spacial allocation problem and deployed it to an AWS lambda function",
     image: ioetLogo,
-    ref: 'https://www.ioet.com/',
+    ref: "https://www.ioet.com/",
     download: false,
-    refText: 'Web',
+    refText: "Web",
   },
-]
+];
 
 export default jobs;
