c63b4503f80f2ad35828a67d56c9892d4b8941cc
SpraylnlPray <danielwildegger@gmx.de>
2020-11-07 20:28:33 +0100

    update meta information

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-import React from 'react';
-import App from './App';
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import React from "react";
+import Favicon from "react-favicon";
+import App from "./App";
 
 ReactDOM.render(
   <HashRouter>
+    <Favicon url={"./icons/favicon.png"} />
     <App />
   </HashRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
