249a9d4068984e9ee0321d98331150f360ed3f8c
SpraylnlPray <danielwildegger@gmx.de>
2021-01-28 15:02:51 +0100

    Try other favicon setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
-import React from "react";
-import Favicon from "react-favicon";
-import App from "./App";
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import React from 'react';
+import Favicon from 'react-favicon';
+import App from './App';
 
 ReactDOM.render(
   <HashRouter>
-    <Favicon url={"./icons/favicon.png"} />
+    <Favicon url='https://raw.githubusercontent.com/SpraylnlPray/MySite/master/src/icons/favicon.png' />
     <App />
   </HashRouter>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
